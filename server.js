69cc8b109b00fbcdbaa9216569884a6590afc5c9
Vivek Ajbe <49584454+vajbe@users.noreply.github.com>
2020-05-29 21:53:20 +0530

    ADding cors entries

--- server.js
+++ server.js
@@ -7,14 +7,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/fastread'));
 
-app.use(function(req, res, next) {
-
-  res.header("Access-Control-Allow-Origin", "*");
-
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-  next();
-
+app.use(function (req, res, next) {
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+res.setHeader('Access-Control-Allow-Credentials', true);
+next();
 });
 
 
